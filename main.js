b20d62f0f638aaa88c305548036faab28be79ab2
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-04-03 01:19:40 -0400

    simple liker done

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector('#modal')
+const errorMessage = document.querySelector('#modal-message')
 
-// Your JavaScript code goes here!
+// Hide the error modal initially
+errorModal.classList.add('hidden')
 
+// Heart elements
+const hearts = document.querySelectorAll('.like-glyph')
 
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
-}
+// Event listener for clicking on a heart
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    // Clear any existing error message
+    errorMessage.textContent = ''
+    
+    // Simulate a server call
+    mimicServerCall()
+      .then(() => {
+        // Server call succeeded
+        if (heart.textContent === EMPTY_HEART) {
+          heart.textContent = FULL_HEART
+          heart.classList.add('activated-heart')
+        } else {
+          heart.textContent = EMPTY_HEART
+          heart.classList.remove('activated-heart')
+        }
+      })
+      .catch(error => {
+        // Server call failed
+        errorMessage.textContent = error
+        errorModal.classList.remove('hidden')
+        setTimeout(() => {
+          errorModal.classList.add('hidden')
+        }, 3000)
+      })
+  })
+})
